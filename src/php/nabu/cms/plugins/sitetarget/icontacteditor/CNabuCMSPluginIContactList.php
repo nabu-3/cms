5bf3b5eeb879547e720537e67ec3e3ddfce0ede6
Rafael Gutiérrez <ragutimar@gmail.com>
2017-12-01 15:02:27 +0100

    Begin implementation of iContact Product

---
+++ src/php/nabu/cms/plugins/sitetarget/icontacteditor/CNabuCMSPluginIContactList.php
@@ -0,0 +1,51 @@
+<?php
+
+/*  Copyright 2009-2011 Rafael Gutierrez Martinez
+ *  Copyright 2012-2013 Welma WEB MKT LABS, S.L.
+ *  Copyright 2014-2016 Where Ideas Simply Come True, S.L.
+ *  Copyright 2017 nabu-3 Group
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace nabu\cms\plugins\sitetarget\icontacteditor;
+use nabu\data\icontact\CNabuIContactList;
+use nabu\http\adapters\CNabuHTTPSiteTargetPluginAdapter;
+
+/**
+ * @author Rafael Gutierrez <rgutierrez@nabu-3.com>
+ * @since 3.0.1 Surface
+ * @version 3.0.2 Surface
+ * @package \nabu\cms\plugins\sitetarget\icontacteditor
+ */
+class CNabuCMSPluginIContactList extends CNabuHTTPSiteTargetPluginAdapter
+{
+    /** @var CNabuIContactList $icontact_list IContact list. */
+    private $icontact_list = null;
+
+    public function prepareTarget()
+    {
+        $this->icontact_list = $this->nb_work_customer->getIContacts(true);
+
+        return true;
+    }
+
+    public function beforeDisplayTarget()
+    {
+        $render = $this->nb_response->getRender();
+        $render->smartyAssign('nb_languages', $this->nb_work_customer->getIContactSetUsedLanguages());
+        $render->smartyAssign('data', $this->icontact_list, $this->nb_language);
+
+        return true;
+    }
+}
